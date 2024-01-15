6a0bd37417e475b4998a03233003942607f65c7b
Bill <bilaljibrilliant@gmail.com>
2024-01-15 14:17:40 +0700

    Language Settings on SignPasswordScreen

--- src/screens/SignInScreen/SignPasswordScreen.js
+++ src/screens/SignInScreen/SignPasswordScreen.js
@@ -6,33 +6,37 @@ import {
   Image,
   Dimensions,
   Alert,
+  ScrollView,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import CustomInput from '../../components/CustomInput';
 import ButtonBack from '../../components/ButtonBack';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {useAuth} from '../../context/AuthContext/AuthContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {URL_API} from '../../../utils';
-
-const langData = require('../../../lang.json');
+import {URL_API, getLanguage} from '../../../utils';
 
 const SignPasswordScreen = () => {
-  const route = useRoute();
   const [lang, setLang] = useState({});
-  const {isLoggedIn, login} = useAuth();
+  const {login} = useAuth();
   const [password, setPassword] = useState('');
   const [userData, setUserData] = useState([]);
-  const {mobile} = route.params;
 
   const navigation = useNavigation();
   let ScreenHeight = Dimensions.get('window').height;
 
   useEffect(() => {
-    const getLanguage = async () => {
+    const fetchData = async () => {
       try {
         const currentLanguage = await AsyncStorage.getItem('currentLanguage');
+        const screenLang = await getLanguage(
+          currentLanguage,
+          'screen_loginWithPassword',
+        );
         const getUserData = await AsyncStorage.getItem('userData');
+
+        // Set your language state
+        setLang(screenLang);
 
         if (getUserData) {
           const userDataObject = JSON.parse(getUserData);
@@ -48,11 +52,6 @@ const SignPasswordScreen = () => {
         } else {
           console.log('Data user tidak ditemukan');
         }
-
-        const selectedLanguage = currentLanguage === 'id' ? 'id' : 'eng';
-        const language = langData[selectedLanguage];
-
-        setLang(language);
       } catch (err) {
         console.error(
           'Error retrieving selfCoordinate from AsyncStorage:',
@@ -61,17 +60,12 @@ const SignPasswordScreen = () => {
       }
     };
 
-    getLanguage();
+    fetchData();
   }, []);
 
   const onSignIn = async () => {
     if (password.trim() === '') {
-      Alert.alert(
-        'Error',
-        lang && lang.screen_signin && lang.screen_signin.alert
-          ? lang.screen_signin.alert.emptyPassword
-          : '',
-      );
+      Alert.alert('Warning', lang.notif.emptyPassword);
     } else {
       try {
         const response = await fetch(
@@ -82,7 +76,7 @@ const SignPasswordScreen = () => {
         console.log('Response Login API -> ' + JSON.stringify(data));
 
         if (data.data === 'false') {
-          Alert.alert('Error', 'Unable to login. Please check your password');
+          Alert.alert('Failed', lang.notif.wrong);
           setPassword('');
         } else {
           await AsyncStorage.setItem('userEmail', userData.email);
@@ -95,7 +89,7 @@ const SignPasswordScreen = () => {
         }
       } catch (error) {
         console.error('Error:', error);
-        Alert.alert('Error', 'An error occurred while logging in');
+        Alert.alert('Error', lang.notif.errorServer);
       }
     }
   };
@@ -105,9 +99,6 @@ const SignPasswordScreen = () => {
   };
 
   const onNotExist = () => {
-    // navigation.navigate('PhoneVerif', {
-    //   mobile: userData.mobile,
-    // });
     navigation.navigate('PhoneLogin', {
       mobile: userData.mobile,
     });
@@ -118,59 +109,69 @@ const SignPasswordScreen = () => {
   };
 
   return (
-    <View style={[styles.root, {height: ScreenHeight}]}>
-      <ButtonBack onClick={onBack} />
+    <ScrollView scrollEnabled={false}>
+      <View style={[styles.root, {height: ScreenHeight}]}>
+        <ButtonBack onClick={onBack} />
 
-      <View style={styles.titleWrapper}>
-        <Text style={styles.title}>Welcome back.</Text>
-        <Text style={styles.subTitle}>
-          Please sign in to continue. {userData.mobile}
-        </Text>
-      </View>
-
-      <CustomInput
-        label={
-          lang && lang.screen_signin && lang.screen_signin.password
-            ? lang.screen_signin.password.label
-            : ''
-        }
-        placeholder={'Please enter your password.'}
-        value={password}
-        setValue={setPassword}
-        secureTextEntry
-        isPassword={true}
-      />
-
-      <View style={[styles.bottomSection]}>
-        <View
-          style={{
-            flexDirection: 'column',
-            alignSelf: 'flex-end',
-            marginBottom: 10,
-            gap: 5,
-          }}>
-          <View style={styles.additionalLogin}>
-            <Pressable onPress={onPassMiss} style={styles.resetPassword}>
-              <Text style={styles.emailAuth}>PASSWORD</Text>
-            </Pressable>
-            <Text style={styles.normalText}> missed.</Text>
-          </View>
-          <View style={styles.additionalLogin}>
-            <Pressable onPress={onNotExist} style={styles.resetPassword}>
-              <Text style={styles.emailAuth}>ACCOUNT</Text>
-            </Pressable>
-            <Text style={styles.normalText}> doesn't exist.</Text>
-          </View>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>
+            {lang && lang.title ? lang.title : ''}
+          </Text>
+          <Text style={styles.subTitle}>
+            {lang && lang.title ? lang.subtitle : ''}
+          </Text>
         </View>
-        <Pressable onPress={onSignIn} style={styles.buttonSignIn}>
-          <Image
-            source={require('../../../assets/images/icon_next.png')}
-            resizeMode="contain"
-            style={styles.buttonSignInImage}
-          />
-        </Pressable>
+
+        <CustomInput
+          label={lang && lang.input ? lang.input.password.label : ''}
+          placeholder={
+            lang && lang.input ? lang.input.password.placeholder : ''
+          }
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+          isPassword={true}
+        />
+
+        <View style={[styles.bottomSection]}>
+          <View
+            style={{
+              flexDirection: 'column',
+              alignSelf: 'flex-end',
+              marginBottom: 10,
+              gap: 5,
+            }}>
+            <View style={styles.additionalLogin}>
+              <Pressable onPress={onPassMiss} style={styles.resetPassword}>
+                <Text style={styles.emailAuth}>
+                  {lang && lang.input ? lang.input.missing.btn + ' ' : ''}
+                </Text>
+              </Pressable>
+              <Text style={styles.normalText}>
+                {lang && lang.input ? lang.input.missing.text : ''}
+              </Text>
+            </View>
+            <View style={styles.additionalLogin}>
+              <Pressable onPress={onNotExist} style={styles.resetPassword}>
+                <Text style={styles.emailAuth}>
+                  {lang && lang.input ? lang.input.notExist.btn + ' ' : ''}
+                </Text>
+              </Pressable>
+              <Text style={styles.normalText}>
+                {lang && lang.input ? lang.input.notExist.text : ''}
+              </Text>
+            </View>
+          </View>
+          <Pressable onPress={onSignIn} style={styles.buttonSignIn}>
+            <Image
+              source={require('../../../assets/images/icon_next.png')}
+              resizeMode="contain"
+              style={styles.buttonSignInImage}
+            />
+          </Pressable>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -198,6 +199,7 @@ const styles = StyleSheet.create({
   },
   bottomSection: {
     padding: 20,
+    paddingBottom: 15,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
