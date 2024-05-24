d6bd8192f9b2b2b5f813573651b91aff97a78bd8
Muhammad Febrian <muhammadfebriyan86@gmail.com>
2024-05-24 08:38:52 +0700

    change time in setTimeout SplashScreen

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,31 +1,37 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect } from "react";
-import { Image, View } from "react-native";
-import Logo from '../../../assets/images/xrun_round.png'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useEffect} from 'react';
+import {Image, View} from 'react-native';
+import Logo from '../../../assets/images/xrun_round.png';
 
 const SplashScreen = ({navigation}) => {
- useEffect(() => {
-	setTimeout(async () => {
-		try {
-			const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+  useEffect(() => {
+    setTimeout(async () => {
+      try {
+        const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
 
-			if(isLoggedIn) {
-				navigation.reset({index: 0, routes: [{name: 'Home'}]});
-				return;
-			}else {
-				navigation.reset({index: 0, routes: [{name: 'First'}]})
-			}
-		}catch(e) {
-			console.log(`Failed get isLoggedIn from AsyncStorage: ${e}`)
-		}
-	}, 100);
- }, []);
- 
- return (
-	<View style={{flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
-	 <Image source={Logo} />
- 	</View>
- )
-}
+        if (isLoggedIn) {
+          navigation.reset({index: 0, routes: [{name: 'Home'}]});
+          return;
+        } else {
+          navigation.reset({index: 0, routes: [{name: 'First'}]});
+        }
+      } catch (e) {
+        console.log(`Failed get isLoggedIn from AsyncStorage: ${e}`);
+      }
+    }, 3000);
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Image source={Logo} />
+    </View>
+  );
+};
 
 export default SplashScreen;
