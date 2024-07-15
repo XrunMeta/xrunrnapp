03be1441ff5e3f0e851a7a24e7c52e4038fccdd4
Rian <muhammadfebriyan86@gmail.com>
2024-07-15 16:19:46 +0700

    remove LogBox for debug

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthProvider} from './src/context/AuthContext/AuthContext';
 import Router from './src/navigation/Router';
-import {LogBox} from 'react-native';
-LogBox.ignoreAllLogs();
 
 export default function App() {
   const [setIsLoggedIn] = useState(false);
