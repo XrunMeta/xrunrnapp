38fcb5268dca9c59cb9f7e47753fd9ae4d638698
Bilal Jibrilliant <bilaljibrilliant@gmail.com>
2024-11-07 15:03:54 +0700

    Fix button position at CustomInputEdit

--- utils.js
+++ utils.js
@@ -3,7 +3,6 @@ import CryptoJS from 'crypto-js';
 import crashlytics from '@react-native-firebase/crashlytics';
 // import messaging from '@react-native-firebase/messaging';
 import * as RNLocalize from 'react-native-localize';
-import React from 'react';
 import CustomInput from './src/components/CustomInput';
 
 // Endpoint API
