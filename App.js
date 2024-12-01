323ca6bf61277f461007a17d81a535624f47170a
XRUN Meta <xrun@xrun.run>
2024-12-01 16:35:16 +0700

    update deeplink, add log at deep link

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthProvider} from './src/context/AuthContext/AuthContext';
 import Router from './src/navigation/Router';
+import { Linking } from 'react-native';
+import { saveLogsDB } from './utils';
 
 export default function App() {
   const [setIsLoggedIn] = useState(false);
@@ -25,6 +27,34 @@ export default function App() {
 
   //   checkLoginStatus();
   // }, []);
+
+  useEffect(() => {
+
+
+const handleDeepLink = (event) => {
+	const url = event.url;
+	console.log("Received url: " + url)
+
+	saveLogsDB(
+		'5071000',
+		0,
+		'User open app by URl Scheme',
+		'User open app by URl Scheme',
+	  );
+}
+
+Linking.addEventListener('url', handleDeepLink);
+
+Linking.getInitialURL().then((url) => {
+	if(url) {
+		handleDeepLink({url})
+	}
+})
+
+return () => {
+	Linking.removeAllListeners('url', handleDeepLink)
+}
+  }, [])
 
   return (
     <AuthProvider>
