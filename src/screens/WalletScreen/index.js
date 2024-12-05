5d71cbc086dce527cc24eb19fc4dc3721b71c656
Rian <muhammadfebriyan86@gmail.com>
2024-12-05 08:37:38 +0700

    Add value for props member ShowQRWallet component

--- src/screens/WalletScreen/index.js
+++ src/screens/WalletScreen/index.js
@@ -525,6 +525,7 @@ const WalletScreen = ({navigation, route}) => {
           cardDataQR={cardDataQR}
           setIsShowQRCodeWallet={setIsShowQRCodeWallet}
           lang={lang}
+          member={member}
         />
       )}
 
