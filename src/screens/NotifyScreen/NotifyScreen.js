879b2afa7edec86ca62640a7d60de2ec5fe09e0d
bi-brilliant <bilaljibrilliant@gmail.com>
2025-01-15 13:52:11 +0700

    Fix isBroadcast placement at NotifyScreen

--- src/screens/NotifyScreen/NotifyScreen.js
+++ src/screens/NotifyScreen/NotifyScreen.js
@@ -65,7 +65,6 @@ const NotifyScreen = () => {
             JSON.stringify({
               type: 'ap6000-01',
               payload: {
-                isBroadcast: false,
                 member: getData?.member,
                 start: 0,
               },
@@ -183,6 +182,7 @@ const NotifyScreen = () => {
           const message = {
             type: 'ap6000-02',
             payload: {
+              isBroadcast: false,
               member: userData?.member,
               title: text,
             },
