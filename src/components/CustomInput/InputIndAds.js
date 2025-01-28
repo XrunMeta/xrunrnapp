7c2530a109e102ca32aa6ffb83ec07bbcade98c6
mfebriann <muhammadfebriyan86@gmail.com>
2025-01-28 16:36:27 +0700

    Make component for input individual ads

---
+++ src/components/CustomInput/InputIndAds.js
@@ -0,0 +1,43 @@
+import {StyleSheet, TextInput} from 'react-native';
+import React from 'react';
+import {fontSize, getFontFam} from '../../../utils';
+
+const InputIndAds = ({value, setValue, placeholder, isEditable = true}) => {
+  return (
+    <TextInput
+      style={{
+        backgroundColor: 'white',
+        paddingHorizontal: 12,
+        marginHorizontal: 8,
+        borderRadius: 10,
+        marginVertical: 4,
+        ...styles.shadow,
+        fontFamily: getFontFam() + 'Regular',
+        fontSize: fontSize('body'),
+        color: 'black',
+      }}
+      value={value}
+      placeholder={placeholder}
+      placeholderTextColor="gray"
+      onChangeText={valueText => {
+        setValue(valueText);
+      }}
+      editable={isEditable}
+    />
+  );
+};
+
+export default InputIndAds;
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.16,
+    shadowRadius: 1.51,
+    elevation: 1,
+  },
+});
