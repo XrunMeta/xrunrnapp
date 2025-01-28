b484076efcf906dac4c21f90d71c37d078a9f0c9
bi-brilliant <bilaljibrilliant@gmail.com>
2025-01-28 11:46:58 +0700

    Modularizing shop screen when controlling each tab item shop

--- src/screens/ShopScreen/ShopScreen.js
+++ src/screens/ShopScreen/ShopScreen.js
@@ -6,9 +6,7 @@ import {
   Dimensions,
   TouchableOpacity,
   Image,
-  ActivityIndicator,
   useWindowDimensions,
-  FlatList,
   SafeAreaView,
   Modal,
   TouchableWithoutFeedback,
@@ -18,458 +16,35 @@ import ButtonBack from '../../components/ButtonBack';
 import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
-import {
-  URL_API_NODEJS,
-  getLanguage2,
-  getFontFam,
-  fontSize,
-  authcode,
-  dateFormatter,
-} from '../../../utils';
+import dataShop from './dataShop.json'; // Import data from JSON file
+import {getLanguage2, getFontFam, fontSize} from '../../../utils';
 import crashlytics from '@react-native-firebase/crashlytics';
+import {itemShopRoutes} from './ItemShop/ItemShopRoutes';
+import {itemShopRenderItems} from './ItemShop/ItemShopRenderItems';
 
 const ShopScreen = () => {
   const [lang, setLang] = useState({});
   const navigation = useNavigation();
   let ScreenHeight = Dimensions.get('window').height;
-  const [userData, setUserData] = useState({});
-  const [completedAds, setCompletedAds] = useState([]);
-  const [completedAdsLoading, setCompletedAdsLoading] = useState(true);
-  const [savedItems, setSavedItems] = useState([]);
-  const [savedItemsLoading, setSavedItemsLoading] = useState(true);
   const [index, setIndex] = useState(0);
+  const [userData, setUserData] = useState({});
+  const [itemShop, setItemShop] = useState([]);
+  const [itemShopLoading, setItemShopLoading] = useState(true);
   const [modalVisible, setModalVisible] = useState(false);
   const [agreementModalVisible, setAgreementModalVisible] = useState(false);
   const [isAgreed, setIsAgreed] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
   const layout = useWindowDimensions();
   const [routes] = useState([
-    {
-      key: 'first',
-    },
-    {
-      key: 'second',
-    },
-    {
-      key: 'third',
-    },
+    {key: 'first', title: 'Saved'},
+    {key: 'second', title: 'Expired'},
+    {key: 'third', title: 'Item Shop'},
   ]);
 
   // Back
   const handleBack = () => {
     navigation.replace('AdvertiseHome');
   };
-
-  useEffect(() => {
-    // Get Language
-    const fetchData = async () => {
-      try {
-        const currentLanguage = await AsyncStorage.getItem('currentLanguage');
-        const screenLang = await getLanguage2(currentLanguage);
-        setLang(screenLang);
-
-        // Get User Data
-        const userData = await AsyncStorage.getItem('userData');
-        const getData = JSON.parse(userData);
-
-        setUserData(getData);
-
-        const response = await fetch(`${URL_API_NODEJS}/app5010-02`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${authcode}`,
-          },
-          body: JSON.stringify({
-            member: getData?.member,
-          }),
-        });
-
-        const data = await response.json();
-
-        if (data && data.data.length > 0) {
-          const filteredAds = data.data.map(ad => {
-            const localizedDatetime = dateFormatter(ad.datetime);
-
-            return {
-              transaction: ad.transaction,
-              title: ad.title,
-              coin: ad.amount + ' ' + ad.symbol,
-              extracode: ad.extracode,
-              datetime: localizedDatetime,
-              statusSuccess:
-                lang && lang.screen_advertise && lang.screen_advertise.completed
-                  ? lang.screen_advertise.completed
-                  : 'Coin acquisition completed',
-              statusPending:
-                lang && lang.screen_advertise && lang.screen_advertise.pending
-                  ? lang.screen_advertise.pending
-                  : 'Waiting for Coin Acquisition',
-            };
-          });
-
-          const items = [
-            {
-              transaction: '1',
-              title: 'Transfer Ticket',
-              price: '$5 / transfer',
-              description: `Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor cubilia suscipit lacus at enim id varius. Ullamcorper maximus netus primis; ultricies pharetra ac. In auctor lacus vitae dignissim ipsum natoque. Gravida ipsum maximus donec cras tellus efficitur consequat elit. Dictum posuere condimentum; rhoncus ante curae volutpat ullamcorper curabitur. Vestibulum diam sagittis et cras vulputate donec.
-\n\nPlatea sollicitudin turpis; hac ex class penatibus feugiat. Hendrerit quam faucibus urna fusce netus non inceptos porta. Volutpat proin massa penatibus amet, velit sem himenaeos. Sed proin neque id magna ullamcorper sagittis habitasse interdum. Ad lobortis fames est dolor, rutrum porta luctus. Eu erat pharetra nostra sagittis suscipit mus ante eleifend. Elit rutrum blandit conubia eu integer neque. Facilisi neque pretium mollis per eu eget. Mauris finibus sollicitudin senectus phasellus auctor justo lobortis.
-\n\nRisus vitae blandit at convallis varius magnis vehicula. Orci duis risus ligula, mattis odio etiam elit faucibus. Dolor massa suscipit leo vestibulum blandit rutrum posuere. Malesuada luctus nam ultrices est congue ut nec convallis. Habitant fames eu facilisis hendrerit volutpat gravida faucibus. Aptent tempus euismod; inceptos eu curae sagittis sociosqu ipsum. Cras at maecenas dis nam ultrices dictumst nulla per. Aptent eu class fringilla massa rhoncus sapien potenti. Netus risus aliquam facilisis pharetra felis ipsum montes hendrerit.
-\n\nSed pharetra eleifend nibh posuere fringilla. Eleifend pharetra mattis velit; platea phasellus accumsan sem ante. Mus vitae turpis bibendum purus habitant sollicitudin velit ad. Sollicitudin commodo natoque sem diam vel mauris orci porta imperdiet. Luctus morbi facilisi curabitur fames dictum nam convallis ligula nascetur. Porta taciti per pellentesque sodales; nunc ligula facilisi nascetur? Nascetur varius et ornare euismod suscipit.
-\n\nEu placerat bibendum dapibus nulla eu risus est. Magnis maecenas tempor velit auctor vestibulum quis senectus. Consectetur nostra id conubia ullamcorper donec. Aptent libero fermentum vehicula habitasse lectus mus. Quis consectetur sociosqu nec odio mus tortor commodo primis. Suscipit nunc ultrices sagittis quis magna. Fringilla cursus aliquam placerat quisque congue massa.`,
-            },
-            {
-              transaction: '2',
-              title: 'Coin Pumper',
-              price: '$15 / 30days',
-              description: `Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor cubilia suscipit lacus at enim id varius. Ullamcorper maximus netus primis; ultricies pharetra ac. In auctor lacus vitae dignissim ipsum natoque. Gravida ipsum maximus donec cras tellus efficitur consequat elit. Dictum posuere condimentum; rhoncus ante curae volutpat ullamcorper curabitur. Vestibulum diam sagittis et cras vulputate donec.`,
-            },
-          ];
-
-          // setCompletedAds(filteredAds);
-          setCompletedAds(items);
-
-          fetchAdsData('datetime', getData.member);
-        }
-
-        setCompletedAdsLoading(false);
-        setSavedItemsLoading(false);
-      } catch (err) {
-        console.error(
-          'Error retrieving selfCoordinate from AsyncStorage:',
-          err,
-        );
-        crashlytics().recordError(new Error(err));
-        crashlytics().log(err);
-        navigation.replace('Home');
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const completedKeyExtractor = (item, index) => item.transaction.toString();
-  const savedItemsKeyExtractor = (item, index) => item.transaction.toString();
-
-  const fetchAdsData = async (orderField, member) => {
-    try {
-      const response = await fetch(`${URL_API_NODEJS}/app5010-01`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${authcode}`,
-        },
-        body: JSON.stringify({
-          member,
-          orderField,
-        }),
-      });
-
-      const data = await response.json();
-
-      if (data) {
-        setSavedItems(data.data);
-      }
-
-      setSavedItemsLoading(false);
-    } catch (err) {
-      console.error('Error fetching ads data:', err);
-      crashlytics().recordError(new Error(err));
-      crashlytics().log(err);
-      navigation.replace('Home');
-    }
-  };
-
-  const onSavedItems = (memberID, advertisement, coin) => {
-    navigation.replace('ShowAd', {
-      screenName: 'AdvertiseHome',
-      member: memberID,
-      advertisement: advertisement,
-      coin: coin,
-      coinScreen: false,
-    });
-  };
-
-  // const completedRenderItem = ({item}) => (
-  //   <View
-  //     style={[styles.list, {display: 'flex', flexDirection: 'row', gap: 10}]}
-  //     key={item.transaction}>
-  //     <View
-  //       style={{
-  //         borderColor: '#d9d9d9',
-  //         borderWidth: 1,
-  //         borderRadius: 5,
-  //         height: 50,
-  //         width: 50,
-  //         alignItems: 'center',
-  //         justifyContent: 'center',
-  //       }}>
-  //       <Image
-  //         source={require('../../../assets/images/logo_xrun.png')}
-  //         resizeMode="contain"
-  //         style={{height: 25}}
-  //       />
-  //     </View>
-  //     <View
-  //       style={{
-  //         flexDirection: 'column',
-  //         justifyContent: 'center',
-  //       }}>
-  //       <Text
-  //         style={[styles.normalText, {color: 'grey'}]}
-  //         ellipsizeMode="tail"
-  //         numberOfLines={1}>
-  //         Transfer Ticket
-  //       </Text>
-  //       <Text style={[styles.normalText, {marginTop: 0, fontWeight: 'bold'}]}>
-  //         $5 / transfer
-  //       </Text>
-  //     </View>
-  //   </View>
-  // );
-
-  const completedRenderItem = ({item}) => (
-    <TouchableOpacity
-      onPress={() => {
-        setSelectedItem(item); // Simpan item yang dipilih
-        setModalVisible(true); // Tampilkan modal
-      }}
-      style={[styles.list, {display: 'flex', flexDirection: 'row', gap: 10}]}
-      key={item.transaction}>
-      <View
-        style={{
-          borderColor: '#d9d9d9',
-          borderWidth: 1,
-          borderRadius: 5,
-          height: 50,
-          width: 50,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Image
-          source={require('../../../assets/images/logo_xrun.png')}
-          resizeMode="contain"
-          style={{height: 25}}
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}>
-        <Text
-          style={[styles.normalText, {color: 'grey'}]}
-          ellipsizeMode="tail"
-          numberOfLines={1}>
-          {item.title}
-        </Text>
-        <Text style={[styles.normalText, {marginTop: 0, fontWeight: 'bold'}]}>
-          $5 / transfer
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
-
-  const savedRenderItem = ({item}) => (
-    <TouchableOpacity
-      onPress={() =>
-        onSavedItems(userData.member, item.advertisement, item.coin)
-      }
-      style={styles.savedItemsList}
-      activeOpacity={0.9}
-      key={item.transaction}>
-      <View
-        style={{
-          flexDirection: 'row',
-          gap: 10,
-          alignItems: 'center',
-        }}>
-        <Image
-          source={{
-            uri: `data:image/jpeg;base64,${item.symbolimg}`,
-          }}
-          style={{
-            height: 35,
-            width: 35,
-            resizeMode: 'contain',
-          }}
-        />
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-          }}>
-          <Text
-            numberOfLines={1}
-            style={[styles.listBigText, {marginBottom: -3}]}>
-            {item.symbol}
-          </Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'flex-end',
-              justifyContent: 'space-between',
-            }}>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'flex-end',
-              }}>
-              <Text
-                style={[
-                  styles.listBigText,
-                  {
-                    marginBottom: -8,
-                    marginTop: -9,
-                  },
-                ]}>
-                {item.amount}
-              </Text>
-              <Text style={styles.listNormalText}> {item.symbol}</Text>
-            </View>
-            <Text style={styles.listNormalText}>
-              {item.dateends}{' '}
-              {lang && lang.screen_advertise && lang.screen_advertise.until
-                ? lang.screen_advertise.until
-                : ''}
-            </Text>
-          </View>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-
-  const savedItemsRoute = () => {
-    return (
-      <View style={{flex: 1}}>
-        {/* List Saved Items */}
-        {savedItemsLoading ? (
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator size="large" color="#343a59" />
-            <Text style={[styles.normalText, {color: 'grey'}]}>
-              {lang && lang.screen_advertise && lang.screen_advertise.loading
-                ? lang.screen_advertise.loading
-                : ''}
-            </Text>
-          </View>
-        ) : savedItems.length === 0 ? (
-          <View style={styles.emptyContainer}>
-            <Text style={styles.emptyText}>
-              {lang && lang.screen_advertise && lang.screen_advertise.nodata
-                ? lang.screen_advertise.nodata
-                : ''}
-            </Text>
-          </View>
-        ) : (
-          <View>
-            <FlatList
-              data={savedItems}
-              keyExtractor={savedItemsKeyExtractor}
-              renderItem={savedRenderItem}
-            />
-          </View>
-        )}
-      </View>
-    );
-  };
-
-  const completedRoute = () => (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      {completedAdsLoading ? (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#343a59" />
-          <Text style={[styles.normalText, {color: 'grey'}]}>
-            {lang && lang.screen_advertise && lang.screen_advertise.loading
-              ? lang.screen_advertise.loading
-              : ''}
-          </Text>
-        </View>
-      ) : completedAds.length === 0 ? (
-        <View style={styles.emptyContainer}>
-          <Text style={styles.emptyText}>
-            {lang && lang.screen_advertise && lang.screen_advertise.nodata
-              ? lang.screen_advertise.nodata
-              : ''}
-          </Text>
-        </View>
-      ) : (
-        <FlatList
-          data={completedAds}
-          keyExtractor={completedKeyExtractor}
-          renderItem={completedRenderItem}
-        />
-      )}
-    </View>
-  );
-
-  const itemShop = () => (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      {completedAdsLoading ? (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#343a59" />
-          <Text style={[styles.normalText, {color: 'grey'}]}>
-            {lang && lang.screen_advertise && lang.screen_advertise.loading
-              ? lang.screen_advertise.loading
-              : ''}
-          </Text>
-        </View>
-      ) : completedAds.length === 0 ? (
-        <View style={styles.emptyContainer}>
-          <Text style={styles.emptyText}>
-            {lang && lang.screen_advertise && lang.screen_advertise.nodata
-              ? lang.screen_advertise.nodata
-              : ''}
-          </Text>
-        </View>
-      ) : (
-        <FlatList
-          data={completedAds}
-          keyExtractor={completedKeyExtractor}
-          renderItem={completedRenderItem}
-        />
-      )}
-    </View>
-  );
-
-  const renderScene = SceneMap({
-    first: () => savedItemsRoute(),
-    second: completedRoute,
-    third: itemShop,
-  });
-
-  const renderTabBar = props => (
-    <TabBar
-      {...props}
-      indicatorStyle={{backgroundColor: '#ffdc04', height: '100%'}}
-      style={{backgroundColor: 'white', elevation: 0}}
-      renderLabel={({route, focused, color}) => (
-        <Text
-          style={{
-            color: focused ? 'black' : 'grey',
-            fontFamily: focused
-              ? getFontFam() + 'Medium'
-              : getFontFam() + 'Regular',
-            fontSize: fontSize('body'),
-            textAlign: 'center',
-          }}>
-          {route.key === 'first'
-            ? 'Saved'
-            : route.key === 'second'
-            ? 'Expired'
-            : route.key === 'third'
-            ? 'Item Shop'
-            : ''}
-        </Text>
-      )}
-    />
-  );
 
   const handleBuyClick = () => {
     setModalVisible(false); // Sembunyikan modal detail
@@ -484,6 +59,78 @@ const ShopScreen = () => {
       setIsAgreed(false);
     }
   };
+
+  const handleItemPress = item => {
+    setSelectedItem(item); // Simpan item yang dipilih
+    setModalVisible(true); // Tampilkan modal
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Get Language
+        const currentLanguage = await AsyncStorage.getItem('currentLanguage');
+        const screenLang = await getLanguage2(currentLanguage);
+        setLang(screenLang);
+
+        // Get User Data
+        const userData = await AsyncStorage.getItem('userData');
+        const getData = JSON.parse(userData);
+        setUserData(getData);
+
+        // Item Shop
+        setItemShop(dataShop);
+        setItemShopLoading(false);
+      } catch (err) {
+        console.error('Error retrieving data from AsyncStorage:', err);
+        crashlytics().recordError(new Error(err));
+        crashlytics().log(err);
+        navigation.replace('Home');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const renderScene = SceneMap({
+    first: () => <Text>Saved Tab</Text>,
+    second: () => <Text>Expired Tab</Text>,
+    third: () =>
+      itemShopRoutes(
+        lang,
+        styles,
+        itemShopLoading,
+        itemShop,
+        item => item.transaction.toString(),
+        ({item, styles, onPress}) =>
+          itemShopRenderItems({
+            item,
+            styles,
+            onPress: () => handleItemPress(item), // Custom onPress handler
+          }),
+      ),
+  });
+
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={{backgroundColor: '#ffdc04', height: '100%'}}
+      style={{backgroundColor: 'white'}}
+      renderLabel={({route, focused}) => (
+        <Text
+          style={{
+            color: focused ? 'black' : 'grey',
+            fontFamily: focused
+              ? getFontFam() + 'Medium'
+              : getFontFam() + 'Regular',
+            fontSize: fontSize('body'),
+            textAlign: 'center',
+          }}>
+          {route.title}
+        </Text>
+      )}
+    />
+  );
 
   return (
     <SafeAreaView style={[styles.root, {height: ScreenHeight}]}>
@@ -653,7 +300,7 @@ const ShopScreen = () => {
                 {/* Modal Desc */}
                 <ScrollView style={styles.modalDescription}>
                   <Text style={[styles.normalText, {color: 'grey'}]}>
-                    {completedAds[0]?.description}
+                    {itemShop[0]?.description}
                   </Text>
                 </ScrollView>
 
